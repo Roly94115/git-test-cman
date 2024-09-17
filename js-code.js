3b7fcc26fdef16db3b3ef2c28d2721e561ba187e
ROLY <rolylove94@gmail.com>
2024-09-17 11:35:08 +0200

    amélioration de la page d'accueil
    -ajout feuille de style
    -ajout java script

---
+++ js-code.js
@@ -0,0 +1,14 @@
+// integer
+let number = 123;
+// Float
+let number2 = 3.14;
+// string
+let userName = 'Foo Bar';
+// boolean
+let jsIsCool = true;
+
+while (true) {
+    console.log('Hello JS World!');
+}
+
+
